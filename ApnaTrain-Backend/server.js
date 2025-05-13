3fc6b08dd5ab8d4e93db27cffa5f3061fd4a27fd
codeAlapan <alapanpal2004@gmail.com>
2025-05-14 03:58:31 +0530

    phase3 testing done

--- ApnaTrain-Backend/server.js
+++ ApnaTrain-Backend/server.js
@@ -9,6 +9,14 @@ const app = express();
 
 app.use(express.json());
 
+
+// Routes:
+const authRoutes = require("./routes/auth.routes.js");
+
+
+
+app.use("/api/auth",authRoutes);
+
 app.listen(process.env.PORT,()=>{
     console.log(`Server running on port ${process.env.PORT}`);
 })
