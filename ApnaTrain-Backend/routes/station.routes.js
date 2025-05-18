23e715afa22e4c2185c4c3c64831e4209fcb0e00
codeAlapan <alapanpal2004@gmail.com>
2025-05-19 02:12:18 +0530

    phase 7 done

--- ApnaTrain-Backend/routes/station.routes.js
+++ ApnaTrain-Backend/routes/station.routes.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const router = express.Router();
 const { addStation, getAllStation } = require("../controllers/station.controller.js");
+const protect = require("../middlewares/auth.middleware.js");
+const authorizedRoles = require("../middlewares/role.middleware.js");
 
-router.post("/add", addStation);
-router.get("/", getAllStation);
+router.post("/add",protect,authorizedRoles('admin'), addStation);
+router.get("/",protect,authorizedRoles('admin','passenger'), getAllStation);
 
 module.exports = router;
