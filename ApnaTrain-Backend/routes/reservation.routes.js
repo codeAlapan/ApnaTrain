a321908b8a3e6b06d9cc0e7056bedf64371701f2
codeAlapan <alapanpal2004@gmail.com>
2025-05-22 04:11:48 +0530

    phase 8 done booking tickets implemented

--- ApnaTrain-Backend/routes/reservation.routes.js
+++ ApnaTrain-Backend/routes/reservation.routes.js
@@ -1,19 +1,18 @@
-const express=require ('express');
-const router=express.Router();
+const express = require('express');
+const router = express.Router();
 const {
-bookTicket,
-getUserReservations,
-cancelReservation
+  bookTicket,
+  getUserReservations,
+  getAllReservations,
+  cancelReservation,
+} = require('../controllers/reservation.controller.js');
 
-}=require('../controllers/reservation.controller.js')
-
-const protect=require('../middlewares/auth.middleware.js')
-const authroizeRoles=require('../middlewares/role.middleware.js')
-
+const protect = require('../middlewares/auth.middleware.js');
+const authorizedRoles = require('../middlewares/role.middleware.js');
 
 router.post('/book', protect, bookTicket);
 router.get('/myreservations', protect, getUserReservations);
 router.delete('/cancel/:id', protect, cancelReservation);
-router.get('/all', protect, authorizeRoles('admin'), getAllReservations);
+router.get('/all', protect, authorizedRoles('admin'), getAllReservations);
 
 module.exports = router;
